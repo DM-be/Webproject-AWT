aefc21c34214778e7ca7fc27858873298d41088f
MorentDennis <dennis.morent@student.howest.be>
2017-12-29 01:38:26 +0100

    fixed old bootstrap messing with layout

--- src/app/ngbd-modal-basic/ngbd-modal-basic.component.ts
+++ src/app/ngbd-modal-basic/ngbd-modal-basic.component.ts
@@ -10,7 +10,7 @@
       closeResult: string;
       constructor(private modalService: NgbModal) {}
       open(content) {
-        this.modalService.open(content).result.then((result) => {
+        this.modalService.open(content, {size: 'lg'}).result.then((result) => {
           this.closeResult = `Closed with: ${result}`;
         }, (reason) => {
           this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
