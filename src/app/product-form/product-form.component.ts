ad14cd5db05986156eb1b0f9105a791cc52bbfce
MorentDennis <dennis.morent@student.howest.be>
2018-01-04 18:40:57 +0100

    code cleanup

--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
+import { Order } from '../order';
 import { HAMBURGERS } from '../mock-hamburgers';
 import { FRIES } from '../mock-fries';
 import { SNACKS } from '../mock-snacks';
 import { SAUCES } from '../mock-sauces';
-
-import { Order } from '../order';
-
-
 
 
 @Component({
@@ -22,41 +19,29 @@ export class ProductFormComponent implements OnInit {
   snacks = SNACKS;
   sauces = SAUCES;
 
-  price:number;
-  en: any;
-
+  price: number;
   selectedFries = new Product("none", 0, 0)
   selectedHamburger = new Product("none", 0, 0)
   selectedSnack = new Product("none", 0, 0)
   selectedSauce = new Product("none", 0, 0)
-
-  model = new Order("", "",  this.selectedHamburger, this.selectedFries, this.selectedSnack, this.selectedSauce);
-  
-  
-  constructor() { }
+  model = new Order("", "", this.selectedHamburger, this.selectedFries, this.selectedSnack, this.selectedSauce);
+  constructor() {}
 
   ngOnInit() {
-    console.log(this.model);
-    
+
   }
 
-  updatePrice() :void
-  { 
+  updatePrice(): void {
     this.price = 0;
     this.price += this.model.hamburger.price
     this.price += this.model.friet.price;
   }
 
-  logObject()
-  {
-    console.log(this.model)
-    var string = JSON.stringify(this.model)
-    JSON.parse(string)
-    console.log(string)
-    console.log(this.model.hamburger.price)
+  logObject() {
   }
 
-  get diagnostic() { return JSON.stringify(this.model);
- 
-
-  }  }
+  get diagnostic() {
+    return JSON.stringify(this.model);
+    
+  }
+}
